c7cc3e03c901613ae0d123d874f593e0a6fce538
Huiwen-git <hwjia@umich.edu>
2020-09-06 19:03:51 -0400

    Add files via upload

--- application.py
+++ application.py
@@ -12,15 +12,19 @@ import os
 import urllib.request
 from flask import Flask, flash, request, redirect, render_template, send_file, send_from_directory
 from werkzeug.utils import secure_filename
+import pandas as pd
+
+
+
 
 
 app = Flask(__name__)
-UPLOAD_FOLDER = '\\uploads\\'
+UPLOAD_FOLDER = 'uploads\\'
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
-file_dir = 'E:\\Huiwen_Jia\\web-app\\uploads\\'
+#file_dir = 'E:\\Huiwen_Jia\\web-app\\uploads\\'
 if not os.path.exists(file_dir):
     os.makedirs(file_dir)
 
@@ -30,19 +34,26 @@ ALLOWED_EXTENSIONS = set(['csv'])
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def generateresult(filename,file_dir):
+    filepath=os.path.join(file_dir, filename)
+    a=pd.read_csv(filepath, header=None)  # 读取训练数据
+    b=2*a
+    filedownloadpath=os.path.join(file_dir, 'download-'+filename.split('.')[0]+'.xlsx')
+    return(b)
+
+#@app.route("/")
+#def home_form():
+#	return render_template('home.html')
 
 
-@app.route("/")
-def home_form():
-	return render_template('home.html')
 
-
-
-@app.route('/upload/')
+@app.route('/')
 def upload_form():
 	return render_template('upload.html')
 
-@app.route('/upload/', methods=['POST'])
+
+
+@app.route('/', methods=['POST'])
 def upload_file():
 	if request.method == 'POST':
         #if 'file' not in request.files:
@@ -55,13 +66,17 @@ def upload_file():
 		if file and allowed_file(file.filename):
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(file_dir, filename))
-			flash('File successfully uploaded')
-			return redirect('/')
+			b=generateresult(filename,file_dir)
+			flash('File successfully uploaded, the page will direct to the result soon!')
+			downloadfilename = 'download'+filename
+			b.to_csv(os.path.join(file_dir, downloadfilename))
+			return render_template('simple.html',  tables=[b.to_html(classes='data')], titles=b.columns.values) #redirect('/download/<downloadfilename>')
 		else:
 			flash('Allowed file types csv')
 			return redirect(request.url)
 
 
+'''
 @app.route("/download/<filename>", methods=['GET'])
 def download_file(filename):
     directory='static\client\csv'
@@ -69,8 +84,7 @@ def download_file(filename):
         return send_from_directory(directory, filename=filename, as_attachment=True)
     except FileNotFoundError:
         abort(404)
-
-
+'''
 
         
 
